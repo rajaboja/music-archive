6226120fe81132f8b72c0a4db6220d227d9e79ed
Rajesh <rajeshbojja@gmail.com>
2024-09-14 21:41:19 +0800

    update logging config

--- config.py
+++ config.py
@@ -7,12 +7,35 @@ import logging
 
 class Config:
     LOGGING_CONFIG = {
-        'level': logging.INFO,
-        'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        'filename': 'app.log',
-        'filemode': 'a'
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'default': {
+                'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            },
+        },
+        'handlers': {
+            'file': {
+                'level': 'INFO',
+                'class': 'logging.FileHandler',
+                'filename': 'app.log',
+                'formatter': 'default',
+            },
+        },
+        'loggers': {
+            '': {
+                'handlers': ['file'],
+                'level': 'INFO',
+                'propagate': True,
+            },
+            'watchfiles.main': {
+                'level': 'WARNING',  # Change the level to WARNING to filter out INFO messages
+                'handlers': ['file'],
+                'propagate': False,
+            },
+        },
     }
 
-    CACHE_DURATION = int(os.getenv('CACHE_DURATION', 3600))  # 1 hour
+    CACHE_DURATION = int(os.getenv('CACHE_DURATION', 3600*24*7))  # 1 hour
     CACHE_FILE = os.getenv('CACHE_FILE', '/tmp/tmk_playlist_cache.json')
     SECRET_KEY = os.getenv('SECRET_KEY', secrets.token_hex(16))
