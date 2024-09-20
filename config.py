13ede129f8d52a9cd206630dadbb4532eac177f7
Rajesh <rajeshbojja@gmail.com>
2024-09-20 23:44:17 +0530

    update the config to use loguru

--- config.py
+++ config.py
@@ -2,42 +2,16 @@
 
 import os
 import secrets
-import logging
-import sys
 import tempfile
+from loguru import logger
 
+# Loguru configuration
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+LOG_FILE = os.getenv("LOG_FILE", "app.log")
+
+logger.add(LOG_FILE, rotation="10 MB", level=LOG_LEVEL)
 
 class Config:
-    LOGGING_CONFIG = {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'formatters': {
-            'default': {
-                'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-            },
-        },
-        'handlers': {
-            'console': {
-                'level': 'INFO',
-                'class': 'logging.StreamHandler',
-                'formatter': 'default',
-                'stream': sys.stdout,
-            },
-        },
-        'loggers': {
-            '': {
-                'handlers': ['console'],
-                'level': 'INFO',
-                'propagate': True,
-            },
-            'watchfiles.main': {
-                'level': 'WARNING',
-                'handlers': ['console'],
-                'propagate': False,
-            },
-        },
-    }
-
     CACHE_DURATION = int(os.getenv('CACHE_DURATION', 3600*24))  # 1 day
     SECRET_KEY = os.getenv('SECRET_KEY', secrets.token_hex(16))
 
