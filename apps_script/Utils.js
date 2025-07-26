3b272b3195a8996f2ce469e9fb3db6b2c9fb47d4
rajaboja <rajeshbojja@gmail.com>
2025-07-26 19:16:03 +0800

    Fix formatDuration function to include single quotes in the returned duration string for consistency

--- apps_script/Utils.js
+++ apps_script/Utils.js
@@ -9,8 +9,8 @@ function formatDuration(isoDuration) {
     const seconds = parseInt(matches[3] || 0);
     
     if (hours > 0) {
-        return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        return `'${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
     } else {
-        return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+        return `'${minutes}:${seconds.toString().padStart(2, '0')}`;
     }
 }
